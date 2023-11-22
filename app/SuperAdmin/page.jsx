a72909f10b38ab32e7c2b61133c7a97ccc395934
najmiddin <turgunpolatov3gmail.com>
2023-11-22 16:42:29 +0500

    commited

--- app/SuperAdmin/page.jsx
+++ app/SuperAdmin/page.jsx
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 import Image from 'next/image'
 import Prokratura from "../../public/mdm++++ 3.png"
 import Prokratura2 from "../../public/Remove-bg.ai_1700556647313.png"
-import Chart from "react-apexcharts";
 import Diagramma from "../../components/Diagramma"
 
 
@@ -53,15 +52,15 @@ export default function page() {
                     <Image src={Prokratura2} width={100} height={100} alt='image' />
                     <Image src={Prokratura} width={200} height={100} alt='image' />
                 </div>
-                {/* 
+
                 <div className="app">
                     <div className="row">
                         <div className="flex justify-center">
                             <Chart options={chartData.options} series={chartData.series} type="bar" width="1300" />
-                        </div> */}
-                <Diagramma />
-                {/* </div>
-                </div> */}
+                        </div>
+                        <Diagramma />
+                    </div>
+                </div>
             </div >
         </>
     )
